1a0938df232822eb29932415d30e45f2d51101e3
bohodirovabdulloh <151772444+bohodirovabdulloh@users.noreply.github.com>
2024-03-18 19:54:33 +0500

    Add files via upload

---
+++ exam-bekzodaka/js/main.js
@@ -0,0 +1,13 @@
+let text = document.querySelector(".number")
+document.getElementById("1").addEventListener("click", function () {
+    text.innerHTML = `машина закрвк`
+});
+document.getElementById("2").addEventListener("click", function () {
+    console.log("машина открыта");
+});
+document.getElementById("3").addEventListener("click", function () {
+    console.log("багаж открыт");
+});
+document.getElementById("4").addEventListener("click", function () {
+    console.log("завад");
+});
